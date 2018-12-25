429dbd03fde42e1c2edd178192deb0eac13bd588
seb hruby <sebastianhruby@gmail.com>
2018-12-25 18:07:15 +0100

    adding simple component into page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/event-list.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule,
